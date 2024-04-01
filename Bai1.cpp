d808945bcd59d47926ef8659e1abd8980702336e
tmi2109 <165612561+tmi2109@users.noreply.github.com>
2024-04-01 08:55:39 +0700

    Add files via upload

---
+++ Bai1.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main(void) {
+    printf("Cac so nguyen co 2 chu so va la boi cua 7: \n");
+    for (int i = 10; i <= 99; i++)
+    {
+        if (i % 7 == 0)
+        {
+            printf("%d, ", i);
+        }
+    }
+    return 0;
+}
